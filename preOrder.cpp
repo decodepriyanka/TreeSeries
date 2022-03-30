953cf3a2fbb8c266c4e360b8040f26dd2bdf5a93
Priyanka Kumari <64683009+decodepriyanka@users.noreply.github.com>
2022-03-31 02:17:15 +0530

    Update preOrder.cpp

--- preOrder.cpp
+++ preOrder.cpp
@@ -1,6 +1,12 @@
----Preorder----
-root->left->right
+// ---Preorder   root->left->right
 
+/* We can solve this using two methods:
+    1)Recursion
+    2)Iteratively
+    */
+
+
+//=================RECURSIVE============================
 
 /**
  * Definition for a binary tree node.
@@ -27,3 +33,29 @@ public:
     }
    
 };
+
+//=============ITERATIVE SOLUTION================
+
+// Create a stack, take the root and push it into the stack , if there exist its left push its left and then its left and so on. When you see there is NULL there is not need to move further
+// Take out the top element of the stack and print it. After that go the right and follow the same process.
+
+// ...........................CODE................
+
+ vector<int> v;
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s;
+        while(root||!s.empty()){
+            if(root!=NULL){
+                s.push(root);
+                root=root->left;
+            }
+            else{
+                root=s.top();
+                s.pop();
+                v.push_back(root->val);
+                root=root->right;
+            }
+          }
+         return v;
+    }
+        
