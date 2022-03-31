adf445d8acdaa100efc6408178ad4098c55e649b
Priyanka Kumari <64683009+decodepriyanka@users.noreply.github.com>
2022-04-01 03:07:43 +0530

    Create maxDepth.cpp

---
+++ maxDepth.cpp
@@ -0,0 +1,22 @@
+Problem link : https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+https://assets.leetcode.com/uploads/2020/11/26/tmp-tree.jpg
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+=============== CODE========================
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        int maxLeft = maxDepth(root->left);
+        int maxRight = maxDepth(root->right);
+        return max(maxLeft, maxRight)+1;
+    }
+};
